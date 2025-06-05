e68fdf2c659759725219d2ce838c05f885edd4ab
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-06-04 21:54:07 -0400

    ult

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -4,9 +4,11 @@ import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './auth/AuthInterceptor';
-const uri = 'https://finalbackactivoscz.fly.dev/graphql';
 
-//const uri = 'http://127.0.0.1:8080/graphql';
+
+//const uri = 'https://finalbackactivoscz.fly.dev/graphql';
+
+const uri = 'http://127.0.0.1:8080/graphql';
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
